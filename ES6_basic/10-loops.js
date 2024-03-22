4581099f1250a06468e5ac78ca518e302d1d4a1e
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-22 14:35:54 -0500

    fixing task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const idx of array) {
-    const value = array[idx];
-    newArray[idx] = appendString + value;
+    newArray[idx] = appendString + idx;
   }
 
   return newArray;
