82107bb89b21841fc40d56958607aae2030b7d5c
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-22 12:26:13 -0500

    Started Task 8, Changing task 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,4 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = { income, gdp, capita };
+    const budget = { a: income, b: gdp, c: capita };
     return budget;
   }
