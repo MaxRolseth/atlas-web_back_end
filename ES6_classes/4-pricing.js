d93c0a9ad348befececa595f14134acd12c04527
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-25 13:37:21 -0500

    Added classes task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+//import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  //getters
+  get amount() {
+    return this._amount;
+  }
+  
+  get currency() {
+    return this._currency;
+  }
+
+  get displayFullPrice() {
+    return this.displayFullPrice();
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+  //setters
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+}
