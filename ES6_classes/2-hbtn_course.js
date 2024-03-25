0f20df35cfa91723b0f74ee45a2833744e95593d
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-25 13:09:26 -0500

    Added classes task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    //verify type
+    if(typeof name !== 'string')
+      throw TypeError('Name must be String');
+    if(typeof length !== 'number')
+      throw TypeError('Length must be Number');
+    if(!Array.isArray(students))
+      throw TypeError('Students must be Array');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  //getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+  //setters
+  set name(name) {
+    if(typeof name !== 'string')
+      throw TypeError('Name must be String');
+
+      this._name = name;
+  }
+  set length(length) {
+    
+    if(typeof length !== 'number')
+      throw TypeError('Length must be Number');
+
+      this._length = length;
+  }
+
+  set students(students) {
+    
+    if(!Array.isArray(students))
+      throw TypeError('Students must be Array');
+
+      this._students = students;
+  }
+}
