52b06ea9668fde16419e8386ff48d9cff779e358
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-25 14:04:53 -0500

    Added classes task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  //getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  //setters
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  
+}
