e657c3e59e92687e2807537e8e27a67f1c731f05
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-25 13:22:17 -0500

    Added classes task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+  //getters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get displayFullCurrency() {
+    return this.displayFullCurrency();
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this.code})`;
+  }
+
+  //setters
+  set code(code) {
+    this._code = code;
+  }
+  
+  set name(name) {
+    this._name = name;
+  }
+}
