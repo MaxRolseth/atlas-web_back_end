a3cc727803a3530b9833775fab38feb9a684d7b7
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-25 11:29:39 -0500

    Added Classes task 1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+import ClassRoom from './0-classrooms.js';
+
+export default function initializeRooms() {
+
+  const roomList = [];
+  const maxStudents = [19, 20, 34];
+
+  for (const num of maxStudents) {
+    const newRoom = new ClassRoom(num);
+    roomList.push(newRoom);
+  }
+  return roomList;
+}
