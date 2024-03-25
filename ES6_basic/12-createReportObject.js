2cf3d9b0c456f9b1998fd61df4161044bb64da93
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-25 09:59:06 -0500

    Added Task 12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+//return all depts and total of depts
+const empReport = {
+  allEmployees: {
+    ...employeesList
+  },
+  getNumberOfDepartments() {
+    return Object.keys(employeesList).length;
+  },
+};
+  return empReport;
+}
