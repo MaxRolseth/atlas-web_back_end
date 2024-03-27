845a9244c8a94cad0631d8046488e328b826efd6
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-27 15:39:38 -0500

    fixed casing

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   if(!map instanceof Map)
-    throw Error('Cannot Process');
+    throw Error('Cannot process');
   for(const [name, value] of map) {
     if(value === 1)
       map.set(name, 100);
