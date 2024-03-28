c90660a6d63b26df101b888badad7657f83b8826
Max Rolseth <max.rolseth@atlasschool.com>
2024-03-28 10:34:58 -0500

    added promise task 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,26 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const resultArray = [];
+
+  try {
+    await signUpUser(fileName, lastName);
+    resultArray.push({
+      status: 'fulfilled',
+      value: { firstName, lastName },
+    });
+
+    const photoResult = await uploadPhoto(fileName);
+    resultArray.push({
+      status: 'fulfilled',
+      value: photoResult,
+    });
+  } catch (error) {
+    resultArray.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+  return resultArray;
+}
